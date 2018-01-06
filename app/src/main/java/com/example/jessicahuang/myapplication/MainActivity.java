2fef34ade539890deab95938bdc12ff50994338f
Jessica <jessicahuangtfg@gmail.com>
2018-01-06 18:36:17 +0800

    Merge_GoogleMap_tfgGG_Finish

--- app/src/main/java/com/example/jessicahuang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jessicahuang/myapplication/MainActivity.java
@@ -1,38 +1,20 @@
 package com.example.jessicahuang.myapplication;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
-
-import static android.R.attr.button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -57,13 +39,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, items);
         MeterSpinner.setAdapter(adapter);
         MeterSpinner.setOnItemSelectedListener();*/
-
-        Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                sendMessage(v);
-            }
-        });
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -97,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
 
                 Intent intent = new Intent( MainActivity.this,GarbageActiviy.class);
+                intent.putExtra("goolgetool", goolgeTool);
                 MainActivity.this.startActivity(intent);
             }
         });
@@ -104,44 +80,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
 
-                Intent intent = new Intent( MainActivity.this,SettingActivity.class);
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+        parking.setOnClickListener(new ImageButton.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent( MainActivity.this, ParkingActivity.class);
+                intent.putExtra("goolgetool", goolgeTool);
                 MainActivity.this.startActivity(intent);
             }
         });
 
     }
 
-    public void sendMessage(View view) {
-
-        Intent intent = new Intent(this, TestActivity.class);
-        intent.putExtra("goolgetool", goolgeTool);
-        startActivity(intent);
-    }
-
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-<<<<<<< HEAD
-*/
 
     protected void onPause() {
         super.onPause();
--- app/src/main/java/com/example/jessicahuang/myapplication/ParkingActivity.java
+++ app/src/main/java/com/example/jessicahuang/myapplication/ParkingActivity.java
@@ -1,6 +1,8 @@
 package com.example.jessicahuang.myapplication;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +10,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class ParkingActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class ParkingActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public GoogleMap mgooglemap;
+    public  GoolgeTool g;
+    public List<Address> addresses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_parking);
 
-        Intent intent = getIntent();
+        g = getIntent().getParcelableExtra("goolgetool");
+        String msg="";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            addresses = geocoder.getFromLocation(g.getLat(), g.getLon(),1);
+            Address address = addresses.get(0);
+
+            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++)
+                msg=msg+address.getAddressLine(i).toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            msg="沒有位置資訊 "+ e.getMessage().toString();
+        }
+
+        TextView parkingnum = (TextView)findViewById(R.id.ParkingNum);
+        TextView addresstxt = (TextView)findViewById(R.id.Address);
+        addresstxt.setText(msg);
+        //TODO:changetxt
+       // parkingnum.setText("經度:"+ g.getLat() + "緯度:" + g.getLon());
         ImageButton home = (ImageButton)findViewById(R.id.home);
         home.setOnClickListener(new ImageButton.OnClickListener(){
             @Override
@@ -25,6 +64,32 @@ public class ParkingActivity extends AppCompatActivity {
             }
         });
 
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        mgooglemap = map;
+        //TODO:Get map road and detail
+        mgooglemap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(g.getLat(), g.getLon()))      // Sets the center of the map to location user
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .build();
+
+        mgooglemap.addMarker(new MarkerOptions()
+                .position(new LatLng(g.getLat(),g.getLon()))
+                .title("現在位置"));
+
+        mgooglemap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
     }
 
